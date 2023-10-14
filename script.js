18e237f121a61c22be258ba757d9cab9db2d4098
Brutols <dtwo97@gmail.com>
2023-10-14 10:48:30 +0200

    first commit js basis excercises

---
+++ script.js
@@ -0,0 +1,38 @@
+let name1 = "john"
+
+let name2 = "John"
+
+console.log(name1 === name2);
+
+console.log(name1.toLowerCase === name2.toLowerCase);
+
+let x = 12
+
+let y = 20
+
+let z = x + y
+
+console.log(z);
+
+console.log(x += y);
+
+console.log(x, y);
+
+console.log(y -= x);
+
+console.log(x, y);
+
+console.log(4 - x);
+
+console.log(y - x);
+
+console.log(x, y);
+
+const daniele = {
+    name: "Daniele",
+    surname: "Motta",
+    age: 26,
+    hobbies: "Gaming, Dancing, Motorbikes"
+}
+
+console.log(daniele);
